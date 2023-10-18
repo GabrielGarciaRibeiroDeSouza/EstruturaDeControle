7251460e715b86dbefb00f37d0ed9fca43bd5612
GabrielGarciaRibeiroDeSouza <49999101+GabrielGarciaRibeiroDeSouza@users.noreply.github.com>
2023-10-18 18:49:34 -0300

    Estrutura de repetição goto/label | modelo de loop

---
+++ Repeticao_goto/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("## Estrutura de repetição com goto/label ##\n");
+
+int i = 1;
+
+//não é recomendado o uso desse modelo de loop, é muito antigo
+repetir:
+
+Console.WriteLine($"i= {i}");
+
+i++;
+
+if (i <= 10)
+{
+    goto repetir;
+}
+
+Console.WriteLine("Fim de processamento...");
+
+Console.ReadLine();
