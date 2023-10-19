1a09870418f449b348d1667cd7e40ccbe33157e9
GabrielGarciaRibeiroDeSouza <49999101+GabrielGarciaRibeiroDeSouza@users.noreply.github.com>
2023-10-19 16:20:44 -0300

    loop infinito com while

--- Repeticao_while/Program.cs
+++ Repeticao_while/Program.cs
@@ -1,5 +1,24 @@
 ﻿Console.WriteLine("## Instrução while ##\n");
 
+while (true)
+{
+    Console.WriteLine("\nInforme um número inteiro: (para sair digite 999)");
+
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero == 999)
+    {
+        break;
+    }
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine($"{numero} é par");
+    }
+    else
+    {
+        Console.WriteLine($"{numero} é impar");
+    }
+}
 
 Console.WriteLine("Fim de processamento");
 Console.ReadLine();
