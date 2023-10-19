a603fcb4c9a6988aa168c5b363416fd702cc0fa5
GabrielGarciaRibeiroDeSouza <49999101+GabrielGarciaRibeiroDeSouza@users.noreply.github.com>
2023-10-19 19:12:33 -0300

    Merge manual do teste1 ao 2

--- Repeticao_while/Program.cs
+++ Repeticao_while/Program.cs
@@ -1,34 +1,34 @@
 ﻿Console.WriteLine("## Instrução while ##\n");
 
+var i = 10;
+while (i > 0)
+{
+Console.WriteLine($"i = {i}");
+i--;
+}
 
+Console.WriteLine("\n----------------------\n");
 
+int numero;
+int contador = 1;
 
+Console.WriteLine("Digite um número maior que zero");
+numero = Convert.ToInt32(Console.ReadLine());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (numero > 0)
+{
+    //loop while
+    Console.WriteLine($"\n## Tabuada do {numero}");
+    while (contador < 11)
+    {
+        Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+        contador++;
+    }
+}
+else
+{
+    Console.WriteLine("\nNúmero deve ser maior que zero!");
+}
 
 Console.WriteLine("\n------------------\n");
 
